d3bcea9296f9170812a6d7e617028a967391d39f
webLashchuk$ git config --global user.name webLashchukgit config --global user.name webLashchuk <chernyadyeviv@gmail.com>
2022-10-27 13:00:22 +0300

    added burger menu, responsive styles

--- app/js/main.js
+++ app/js/main.js
@@ -8,12 +8,52 @@ $(function () {
         }
     });
 
+    $('.burger-btn, .header-menu__link').on('click', function () {
+        $('.logo').toggleClass('logo__menu');
+        $('.header-menu__list').toggleClass('burger__menu');
+        $('.header-menu__item').toggleClass('burger__menu__item');
+        $('.mobile-menu').toggleClass('mobile-menu--active');
+        $('body').toggleClass('lock');
+    })
+
     const headerHeight = $('.header').outerHeight();
 
-    $('.reviews-slider').slick({
-        dots:true,
-        infinite: true
+    $('.reviews-slider__list').slick({
+        dots: true,
+        infinite: true,
+        adaptiveHeight: true,
+        appendArrows: $('.reviews-slider__controls'),
+        appendDots: $('.reviews-slider__controls'),
+        responsive:[
+            {
+                breakpoint: 560,
+                settings: {
+                    dots: false
+                }
+            }
+        ]
     });
+
+    const mediaQuery = window.matchMedia('(max-width: 768px)')
+    
+    function handleTableChange(e) {
+        if (e.matches) {
+            $('.restaurants__list').slick({
+                arrows: false,
+                dots: true,
+                infinite: true,
+                slidesToShow: 1,
+                adaptiveHeight: true,
+                appendDots: $('.restaurants__controls-dots')
+            })
+        } else {
+            $('.restaurants__list').slick('unslick');
+        }
+    }
+
+
+    mediaQuery.addListener(handleTableChange);
+    handleTableChange(mediaQuery);
 
     let mixer = mixitup('.food', {
         controls: {
